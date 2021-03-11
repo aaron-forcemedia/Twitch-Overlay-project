a2ea7704b322b13d0d6d1182b8618d73d1e477d5
Aaron Wilson <aaron.wilson6283@gmail.com>
2021-03-10 21:29:03 -0500

    Got it working with Javascript

--- simulator.js
+++ simulator.js
@@ -1,12 +1,52 @@
 
 function follow() {
     
-    function show() {
-        document.getElementById("follow_gif").style.display = "block";
-        setTimeout("hide()", 5000);  // 5 seconds
-    }
-
     function hide() {
         document.getElementById("follow_gif").style.display = "none";
     }
+    
+    function show() {
+        document.getElementById("follow_gif").style.display = "block";
+        setTimeout(hide, 5000);  // 5 seconds
+     }
+    show();
+}
+
+function subscribe() {
+    
+    function hide() {
+        document.getElementById("subscribe_gif").style.display = "none";
+    }
+    
+    function show() {
+        document.getElementById("subscribe_gif").style.display = "block";
+        setTimeout(hide, 5000);  // 5 seconds
+     }
+    show();
+}
+
+function like() {
+    
+    function hide() {
+        document.getElementById("like_gif").style.display = "none";
+    }
+    
+    function show() {
+        document.getElementById("like_gif").style.display = "block";
+        setTimeout(hide, 5000);  // 5 seconds
+     }
+    show();
+}
+
+function shoutout() {
+    
+    function hide() {
+        document.getElementById("shoutout_gif").style.display = "none";
+    }
+    
+    function show() {
+        document.getElementById("shoutout_gif").style.display = "block";
+        setTimeout(hide, 5000);  // 5 seconds
+     }
+    show();
 }
