fd164f2121a3421b850c286e1f526b1d3964d4c0
Aaron Wilson <aaron.wilson6283@gmail.com>
2021-03-25 15:57:24 -0400

    Loops added

--- simulator.js
+++ simulator.js
@@ -7,13 +7,16 @@ function hide_follow() {
     document.getElementById("follow_txt").style.display = "none";
 }
 
+let followcounter = 0;
+
 function follow() {
+  while (followcounter <1){
+
     document.getElementById("follow_gif").style.display = "block";
     setTimeout(hide_follow, 5000);  // 5 seconds
     
     document.getElementById("follow_txt").style.display = "block";
     setTimeout(hide_follow, 5000);  // 5 seconds
-
 
     var para = document.createElement("span");          //Help from W3Schools to understand this section
     var node = document.createTextNode(displayName);
@@ -21,18 +24,23 @@ function follow() {
 
     var element = document.getElementById("follow_txt");
     element.appendChild(para);
-}
+    followcounter++;
+  return followcounter;
+}}
 //-----------------------------------
 function hide_subscribe() {
     document.getElementById("subscribe_gif").style.display = "none";
     document.getElementById("subscribe_txt").style.display = "none";
 }
 
+let subscribecounter = 0;
+
 function subscribe() {
+  while (subscribecounter <1){
+
     document.getElementById("subscribe_gif").style.display = "block";
     setTimeout(hide_subscribe, 5000);  // 5 seconds
-    
-    
+        
     document.getElementById("subscribe_txt").style.display = "block";
     setTimeout(hide_subscribe, 5000);  // 5 seconds
 
@@ -42,17 +50,22 @@ function subscribe() {
 
     var element = document.getElementById("subscribe_txt");
     element.appendChild(para);
-}
+    subscribecounter++;
+    return subscribecounter;
+}}
 //-----------------------------------
 function hide_like() {
     document.getElementById("like_gif").style.display = "none";
     document.getElementById("like_txt").style.display = "none";
 }
 
+let likecounter = 0;
+
 function like() {
+  while (likecounter < 1){
+
     document.getElementById("like_gif").style.display = "block";
     setTimeout(hide_like, 5000);  // 5 seconds
-    
     
     document.getElementById("like_txt").style.display = "block";
     setTimeout(hide_like, 5000);  // 5 seconds
@@ -63,25 +76,13 @@ function like() {
 
     var element1 = document.getElementById("like_txt");
     element1.appendChild(para1);
-}
-//-----------------------------------
-function hide_shoutout() {
-    document.getElementById("shoutout_gif").style.display = "none";
-    document.getElementById("shoutout_txt").style.display = "none";
-}
+    likecounter++;
+    return likecounter;
+}}
 
-function shoutout() {
-    document.getElementById("shoutout_gif").style.display = "block";
-    setTimeout(hide_shoutout, 5000);  // 5 seconds
-    
-    
-    document.getElementById("shoutout_txt").style.display = "block";
-    setTimeout(hide_shoutout, 5000);  // 5 seconds
-
-    var para = document.createElement("span");
-    var node = document.createTextNode(displayName);
-    para.appendChild(node);
-
-    var element = document.getElementById("shoutout_txt");
-    element.appendChild(para);
+function info() {
+    hide_like();
+    hide_subscribe();
+    hide_follow();
+    alert('The purpose of this application is to simulate a Twitch Overlay. The background will be transparent on the overlay and will be dropped on top of a live gaming stream via OBS (Open Broadcaster Software) Eventually, the idea is to connect this to a Twitch Chat API to collect variables and create on screen effects. Currently, a prompt is collecting the UserName variable. To simulate spam prevention the app only allows for each action to occur once.');
 }
