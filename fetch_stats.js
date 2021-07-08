17fd25a1f645278e39058e3f37f2f4f11c7e5898
Aaron Wilson <aaron.wilson6283@gmail.com>
2021-07-08 15:41:01 -0400

    javascript 2 first update

---
+++ fetch_stats.js
@@ -0,0 +1,55 @@
+const fetch = require("node-fetch");
+var userName = () => prompt ('What is your user name?');
+const nameInput = userName();
+
+
+// This is the fetchStats method - which is an async function
+const fetchStats = async function(playerName) {
+  let response = await fetch(`https://nykloo.com/api/PlayerInfos/Search?usernameQuery=${playerName}&page=0&pageSize=25`)
+  if (response.status !== 200) {
+    throw new Exception('Looks like there was a problem. Status Code: ' + response.status)
+  } else {
+    let responseJson = await response.json()
+    idName = responseJson[0]['playFabId']
+
+    let statsResponse = await fetch(`https://nykloo.com/api/PlayerStats/Stats/${idName}`)
+
+    if (statsResponse.status !== 200) {
+      throw new Exception('Looks like there was a problem. Status Code: ' + statsResponse.status)
+    }
+    let statsResponseJson = await statsResponse.json()
+
+    return statsResponseJson
+  }
+}
+
+let nameInput = prompt('Enter Player Name: ');
+
+// Now we we will simply call this function
+fetchStats('nameInput')
+  .then((stats) => {
+    //console.log(stats)
+    playerStats = stats['playerStatistics']
+    //console.log(playerStats)
+    playerStats.forEach(function(stat) {
+      // console.log('Current stat: ', stat)
+      if (stat['statisticName'] == 'PlayerSkill') {
+        console.log('Player Skill: ' + stat['value'])
+      } else {        
+      if (stat['statisticName'] == 'CareerKills') {
+        console.log('Career Kills: ' + stat['value'])
+      } else {
+      if (stat['statisticName'] == 'CareerWins') {
+        console.log('Career  Wins: ' + stat['value'])
+      } else {
+      if (stat['statisticName'] == 'CareerGamesPlayed') {
+        console.log('Career Games: ' + stat['value'])
+      } else {
+        // Do something with the others?
+        //console.log(stat['statisticName'])
+      }}}}
+    });
+  })
+  .catch((e) => {
+    console.log(e)
+  })
